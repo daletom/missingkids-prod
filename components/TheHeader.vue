3d344b68612a9e4a2f3cdf7845426d9369e4a1ff
daletom <tom@imgix.com>
2020-11-23 09:41:56 -0800

    clone for prod

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,20 +1,20 @@
 <template>
-  <header class="flex items-center justify-between flex-wrap bg-black p-6">
-    <NuxtLink to="/"><Logo class="mb-0 mr-6" /></NuxtLink>
+  <header class="flex items-center justify-between flex-wrap shadow-2xl bg-gray-700 p-6">
+    <NuxtLink to="/" class="no-underline mr-2 block mt-4 sm:inline-block border-white border-2 border-double rounded-t-lg sm:mt-0 text-white hover:text-yellow-400" >Home</NuxtLink>
     <div class="block sm:hidden">
       <button @click="toggle" class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-100 hover:text-white hover:border-white">
         <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
       </button>
     </div>
-    <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
+    <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto pl-4">
       <div class="text-sm sm:flex-grow">
-        <nuxt-link to="/help" class="no-underline mr-2 block mt-4 sm:inline-block sm:mt-0 text-white hover:text-yellow-400">
+        <nuxt-link to="/help" class="border-white border-2 border-double rounded-l-lg no-underline mr-2 block mt-4 sm:inline-block sm:mt-0 text-white hover:text-yellow-400">
           Ways to Help
         </nuxt-link>
-        <a href="/about" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-white hover:text-yellow-400 mr-4">
+        <a href="/about" class="border-white border-2 border-double no-underline block mt-4 sm:inline-block sm:mt-0 text-white hover:text-yellow-400 mr-4">
           About Project
         </a>
-        <a href="/aboutme" class="no-underline mr-2 block mt-4 sm:inline-block sm:mt-0 text-white hover:text-yellow-400">
+        <a href="/aboutme" class="border-white border-2 border-double rounded-r-lg no-underline mr-2 block mt-4 sm:inline-block sm:mt-0 text-white hover:text-yellow-400">
           About Author
         </a>
       </div>
